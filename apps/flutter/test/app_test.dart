c6303ac2beb66381dad1c774d23750c4c141869e
Mohammad Farooq <babaqatar@gmail.com>
2026-09-04 20:24:10 +0300

    Test responsive Flutter workspace

--- apps/flutter/test/app_test.dart
+++ apps/flutter/test/app_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('shows the FarooqDrive workspace', (tester) async {
     await tester.pumpWidget(const FarooqDriveApp());
-    expect(find.text('FarooqDrive'), findsOneWidget);
-    expect(find.text('Add Google account'), findsOneWidget);
+    expect(find.text('All Drives'), findsOneWidget);
+    expect(find.text('Search this folder'), findsOneWidget);
   });
 }
