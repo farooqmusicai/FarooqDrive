b61026a74cd5dd2edc144fee4d5e9cc8133b577a
Mohammad Farooq <babaqatar@gmail.com>
2026-09-05 13:14:25 +0300

    Update navigation label test

--- apps/flutter/test/app_test.dart
+++ apps/flutter/test/app_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('shows the FarooqDrive workspace', (tester) async {
     await tester.pumpWidget(const FarooqDriveApp());
-    expect(find.text('All Drives'), findsOneWidget);
+    expect(find.text('All Drives'), findsWidgets);
     expect(find.text('Search all Drives'), findsOneWidget);
   });
 }
